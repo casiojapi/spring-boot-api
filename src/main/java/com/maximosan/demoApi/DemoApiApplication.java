6bee6278722fac0c659d96c128256d0b52b38c2d
maximosan <maximosantoro@protonmail.com>
2021-07-13 20:07:32 -0300

    set up dependecies - sql jpa

--- src/main/java/com/maximosan/demoApi/DemoApiApplication.java
+++ src/main/java/com/maximosan/demoApi/DemoApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.maximosan.demoApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApiApplication.class, args);
 	}
 
 }
